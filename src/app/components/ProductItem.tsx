da1dd56ff98d769c8e9722534a0d78901656210b
Muhammed-gamal22 <midogemi47@gmail.com>
2025-10-06 23:06:21 +0300

    save

--- src/app/components/ProductItem.tsx
+++ src/app/components/ProductItem.tsx
@@ -5,22 +5,27 @@ const ProductItem = ({item}:any) => {
   return (
      <div className='h-[384px]'>
        <div className='border p-3 border-[rgba(0, 0, 0, 0.05)] rounded-[20px]'>
-        <div className='flex gap-2 items-center justify-end'>
+         <div className='flex items-center justify-between'>
+          {item.discount && <div className='w-[74px] rounded-md rou text-[#BE968E] px-2 border border-[#4040401A] h-[30px] flex items-center justify-center'>
+            <span>{item.discount}</span>
+          </div>}
+         <div className='flex gap-2  w-full items-center justify-end'>
               <div className='border w-[36px] h-[36px] 
               flex items-center justify-center 
               border-[rgba(255, 255, 255, 0.3)]
               rounded-[8px]
               '>
-              <Image src={"/bag-add.svg"} alt="" width={24} height={24}/>
+              <Image src={item.icon.bag} alt="" width={24} height={24}/>
               </div>
               <div className='border gap-2 w-[36px] h-[36px] 
               flex items-center justify-center 
               border-[rgba(255, 255, 255, 0.3)]
               rounded-[8px]'>
-              <Image src={"/love.svg"} alt="" width={24} height={24}/>
+              <Image src={item.icon.love} alt="" width={24} height={24}/>
               </div>
         </div>
-        <Image src={"/photo1.svg"} alt="" width={198} height={198}/>
+         </div>
+        <Image className='mx-auto' src={item.image} alt="" width={198} height={198}/>
          
     </div>
     <div>
@@ -28,15 +33,15 @@ const ProductItem = ({item}:any) => {
             <span className='text-[12px] text-[rgba(84, 84, 84, 1)]'>{item.type}</span>
             <div className='flex items-center gap-2'>
                <Image alt="" src={"/star.svg"} width={16} height={16}/>
-               <span className='text-[12px] font-bold text-[rgba(2, 2, 2, 1)]'>4.9</span>
-               <span className='text-[rgba(84, 84, 84, 1)] text-[10px]'>(120)</span>
+               <span className='text-[12px] font-bold text-[rgba(2, 2, 2, 1)]'>{item.rating}</span>
+               <span className='text-[rgba(84, 84, 84, 1)] text-[10px]'>({item.num})</span>
             </div>
             </div>
-            <p className='text-[14px] font-medium text-[rgba(2, 2, 2, 1)]'>J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yello ..</p>
+            <p className='text-[14px] font-medium text-[rgba(2, 2, 2, 1)]'>{item.desc}</p>
             <div className='flex justify-between mt-[10px] w-full'>
             <p className='text-[14px] font-medium text-[rgba(2, 2, 2, 1)]'>
-              <span>AED</span>
-              <span>900</span>
+              <span>{item.priceUnit}</span>
+              <span>{item.price}</span>
             </p>
             <div className='flex gap-2'>
               <div className='w-[16px] h-[16px] bg-[#be968e] rounded-[8px]'></div>
--- src/app/components/SimilarItems.tsx
+++ src/app/components/SimilarItems.tsx
@@ -11,27 +11,79 @@ const ITEMS=[
     {
       image:"/photo1.svg",
       type:"Dresses",
-    } ,
+      rating:"4.9",
+      desc:"J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yellow Stripe",
+      price:"900",
+      icon:{
+        love:"/love-icon.svg",
+        bag:"/bag-add.svg"
+      }
+    },
     {
         image:"/photo1.svg",
         type:"Dresses",
+        rating:"4.9",
+        desc:"J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yellow Stripe",
+        price:"900",
+        priceUnit:"AED",
+        discount:"20%OFF",
+        icon:{
+            love:"/love-icon.svg",
+            bag:"/bag-add.svg"
+          }
   
       } ,
       {
         image:"/photo1.svg",
+        rating:"4.9",
+        desc:"J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yellow Stripe",
+        price:"900",
+        priceUnit:"AED",
+        num:"120",
         type:"Dresses",
+        icon:{
+            love:"/love-icon.svg",
+            bag:"/bag-add.svg"
+          }
       },
       {
         image:"/photo1.svg",
+        rating:"4.9",
+        priceUnit:"AED",
+        desc:"J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yellow Stripe",
+        price:"900",
+        num:"120",
         type:"Dresses",
+        icon:{
+            love:"/love-icon.svg",
+            bag:"/bag-add.svg"
+          }
       },
       {
         image:"/photo1.svg",
+        rating:"4.9",
+        priceUnit:"AED",
+        desc:"J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yellow Stripe",
+        price:"900",
+        num:"120",
         type:"Dresses",
+        icon:{
+            love:"/love-icon.svg",
+            bag:"/bag-add.svg"
+          }
       },
       {
         image:"/photo1.svg",
+        rating:"4.9",
+        priceUnit:"AED",
+        num:"120",
         type:"Dresses",
+        desc:"J.VER Women's Dress Shirts Solid Long Sleeve Stretch Wrinkle-Free With Yellow Stripe",
+        price:"900",
+        icon:{
+            love:"/love-icon.svg",
+            bag:"/bag-add.svg"
+          }
       }        
 
 
@@ -39,7 +91,8 @@ const ITEMS=[
 const SimilarItems = () => {
   const [swiper,setSwiper]=useState<SwiperType | null>(null)
     return (
-        <div className="py-4 ml-[10%] mx-auto">
+        <div className="py-4 ">
+          <div className="w-[90%] mx-auto">
    <div className="relative mb-2">
   <Image 
     src="/layer-1.svg" 
@@ -75,7 +128,7 @@ const SimilarItems = () => {
             <ProductItem key={item.image} item={item}/>
             </SwiperSlide>
          ))}
-         <div className="flex justify-center gap-2">
+         <div className="flex mt-[22px] justify-center gap-2">
          <div onClick={()=>swiper?.slidePrev()} className="w-[50px] cursor-pointer bg-[#E8EDF2] rounded-full h-[50px] flex items-center justify-center">
              <Image src={"/group-1.svg"} alt="" width={24} height={24}/>
           </div>
@@ -84,6 +137,7 @@ const SimilarItems = () => {
           </div>
          </div>
       </Swiper>
+        </div>
         </div>
         </div>
     )
